b6fbe80d5a0c078a6e5eca888e809b4f0cd9f741
Eloy Bartolome Garcia Gonzalez <eloy.garcia@TOOLFACTORY.NET>
2018-07-18 09:30:42 +0200

    Adding test project and some tests

---
+++ BlueClassCompared.Tests/BasicTypeCompared.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlueClassCompared.Tests
+{
+    [TestClass]
+    public class BasicTypeCompared
+    {
+        [TestMethod]
+        public void We_Can_compare_two_string_abs()
+        {
+            var firstString = "aeiou";
+            var secondString = "aeiou";
+            Assert.IsTrue(ClassCompared.AbsoluteCompareResult(firstString, secondString));
+        }
+
+        [TestMethod]
+        public void If_we_compare_empty_vs_null_in_absolutecompareresult_we_have_false()
+        {
+            var firstString = string.Empty;
+            Assert.IsFalse(ClassCompared.AbsoluteCompareResult(firstString, null));
+        }
+
+        [TestMethod]
+        public void If_we_compare_emtpty_vs_null_in_compareproperties_we_have_true()
+        {
+            var firstString = string.Empty;
+            Assert.IsTrue(ClassCompared.ComparePropertiesResult(firstString, null));
+        }
+
+        [TestMethod]
+        public void If_we_compare_two_diferent_int_we_get_always_false()
+        {
+            int? firstInt = 5;
+            int? secondInt = 0;
+            Assert.IsFalse(ClassCompared.ComparePropertiesResult(firstInt, secondInt));
+            Assert.IsFalse(ClassCompared.AbsoluteCompareResult(firstInt, secondInt));
+            Assert.IsFalse(ClassCompared.ComparePropertiesResult(null, secondInt));
+            Assert.IsFalse(ClassCompared.AbsoluteCompareResult(null, secondInt));
+        }
+
+
+        [TestMethod]
+        public void If_we_compare_two_diferent_list_of_string_with_AbsoluteCompare_only_get_true_if_all_items_are_equal()
+        {
+            var firstListString = new List<string>(){"a", "ei", "ou"};
+            var secondListString = new List<string>() { "a", "ei", "ou" };
+            var firstListStringA = new List<string>() { "a", "ei", "ou", string.Empty };
+            var secondListStringA = new List<string>() { "a", "ei", "ou", null };
+
+            Assert.IsTrue(ClassCompared.AbsoluteCompareResult(firstListString, secondListString));
+            Assert.IsFalse(ClassCompared.AbsoluteCompareResult(firstListStringA, secondListStringA));
+        }
+
+
+        [TestMethod]
+        public void If_we_compare_two_diferent_list_of_string_with_CompareProperties_only_get_true_if_all_items_are_equal_or_one_is_null_and_other_is_empty()
+        {
+            var firstListString = new List<string>() { "a", "ei", "ou" };
+            var secondListString = new List<string>() { "a", "ei", "ou" };
+            var firstListStringA = new List<string>() { "a", "ei", "ou", string.Empty };
+            var secondListStringA = new List<string>() { "a", "ei", "ou", null };
+
+            Assert.IsTrue(ClassCompared.ComparePropertiesResult(firstListString, secondListString));
+            Assert.IsTrue(ClassCompared.ComparePropertiesResult(firstListStringA, secondListStringA));
+        }
+    }
+}
---
+++ BlueClassCompared.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlueClassCompared.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlueClassCompared.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c55896f2-2e63-4f13-acfc-b7274f454036")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
